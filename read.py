d32532ac29b76d344ab496949c15c7cbf21f83b2
Odysseas <podysseas@auth.gr>
2020-03-18 03:32:39 +0200

    #

--- read.py
+++ read.py
@@ -2,13 +2,15 @@
 
 ------------------- READ FILES ---------
 
-
 '''
-
 # split lines removes "\n"
 # at the end of every line 
 with open("a_solar.txt", 'rb') as f:
     f_contents  = f.read().splitlines()
     print(f_contents)
 
-
+# example access an element 
+# decode utf-8 is important 
+# this is why : b"###"
+    
+f_contents[0].decode("utf-8")
