55a40289d7658761333790bd4ec20f9de28f3e09
Odysseas <podysseas@auth.gr>
2020-03-18 16:45:47 +0200

    #

--- read.py
+++ read.py
@@ -1,7 +1,12 @@
 '''
-
 ------------------- READ FILES ---------
 
+output files:
+    
+        width       : integer
+        height      : integer
+        temp_map    : list with dimensions  --> height * width 
+        f_contents  : i-index is the i-th line of our file ( starts from 0 )
 '''
 # split lines removes "\n"
 # at the end of every line 
@@ -13,4 +18,12 @@ with open("a_solar.txt", 'rb') as f:
 # decode utf-8 is important 
 # this is why : b"###"
     
-f_contents[0].decode("utf-8")
+dimensions  = ( f_contents[0].decode("utf-8") ).split()
+
+# index in Python starts from zero 
+# our data is in a list
+
+width      = int ( dimensions[0] ) ;
+height     = int ( dimensions[1] ) ;
+
+our_map = f_contents[1:(height+1)]
